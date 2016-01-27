a45786aa20d9fbf7995aaf7dc7db73d2867073c5
Ashish Khanna <singh.surv@neu.husky.edu>
2016-01-27 14:26:39 -0500

    Layout

--- server.js
+++ server.js
@@ -1,159 +1,109 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var app = express();
 
+app.use(express.static(__dirname + '/public'));
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+var s = "<!DOCTYPE html>";
+s += "<html lang='en'>";
+s += "<head>";
+s += "  <title>My Form</title>";
+s += "  <meta charset='utf-8'>";
+s += "  <meta name='viewport' content='width=device-width, initial-scale=1'>";
+s += "  <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>";
+s += "  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js'></script>";
+s += "  <script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>";
+s += "</head>";
+s += "<body>";
+s += "";
+s += "<div class='container'>";
+s += "  <h1>PART 6. Programming Assignment </h1>";
+s += "  <h2>1. General Information </h2>";
+s += "  <h3>Your Information </h3>";
+s += "  <form class='form-horizontal' role='form' action='getForm' method='post'>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='name'>Name:</label>";
+s += "      <div class='col-sm-10'>";
+s += "        <input type='text' class='form-control' name='name' placeholder='Enter Name'>";
+s += "      </div>";
+s += "    </div>";
+s += "	<div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='address'>Address:</label>";
+s += "      <div class='col-sm-10'>";
+s += "        <input type='text' class='form-control' name='address' placeholder='Enter address'>";
+s += "      </div>";
+s += "    </div>";
+s += "	<div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='city'>City, State, Zip:</label>";
+s += "      <div class='col-sm-10'>";
+s += "        <input type='text' class='form-control' name='city' placeholder='Enter City, State, Zip'>";
+s += "      </div>";
+s += "    </div>";
+s += "	<div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='phone'>Phone:</label>";
+s += "      <div class='col-sm-10'>";
+s += "        <input type='text' class='form-control' name='phone' placeholder='Enter phone'>";
+s += "      </div>";
+s += "    </div>";
+s += "";
+s += "	<div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='email'>Email:</label>";
+s += "      <div class='col-sm-10'>";
+s += "        <input type='email' class='form-control' name='email' placeholder='Enter email'>";
+s += "      </div>";
+s += "    </div>";
+s += "	<h3>Pet Information </h3>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='account'>Account Number:</label>";
+s += "      <div class='col-sm-10'>          ";
+s += "        <input type='text' class='form-control' name='account' placeholder='Enter Account Number'>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='petName'>Name:</label>";
+s += "      <div class='col-sm-10'>          ";
+s += "        <input type='text' class='form-control' name='petName' placeholder='Enter Name:'>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='breed'>Breed:</label>";
+s += "      <div class='col-sm-10'>          ";
+s += "        <input type='text' class='form-control' name='breed' placeholder='Enter Breed:'>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='age'>Age:</label>";
+s += "      <div class='col-sm-10'>          ";
+s += "        <input type='text' class='form-control' name='age' placeholder='Enter Age:'>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>";
+s += "      <label class='control-label col-sm-2' for='gender'>Gender:</label>";
+s += "      <div class='col-sm-10'>          ";
+s += "        <input type='text' class='form-control' name='gender' placeholder='Enter gender:'>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>        ";
+s += "      <div class='col-sm-offset-2 col-sm-10'>";
+s += "        <div class='checkbox'>";
+s += "          <label><input type='checkbox'> Remember me</label>";
+s += "        </div>";
+s += "      </div>";
+s += "    </div>";
+s += "    <div class='form-group'>        ";
+s += "      <div class='col-sm-offset-2 col-sm-10'>";
+s += "        <button type='submit' class='btn btn-default'>Submit</button>";
+s += "      </div>";
+s += "    </div>";
+s += "  </form>";
+s += "</div>";
+s += "";
+s += "</body>";
+s += "</html>";
 
-    //  Scope.
-    var self = this;
+app.get('/test', function(req, res){
+    res.send(s);
+});
 
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+app.listen(3000);
