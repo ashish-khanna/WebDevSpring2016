3614fcbfa80414f22ee88e088ca78296595d8133
Ashish Khanna <singh.surv@neu.husky.edu>
2016-03-06 14:29:14 -0500

    Routing Implementation

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -2,11 +2,36 @@
  * Created by Ashish on 3/3/2016.
  */
 (function(){
-
     var app = angular
-        .module("FormApp", []);
-
-    app.controller("TestController", testController);
+        .module("FormApp", ["ngRoute"])
+        .controller("TestController", testController)
+        .config(function ($routeProvider){
+            $routeProvider
+                .when("/", {
+                    templateUrl : "views//home/home.view.html"
+                })
+                .when("/home", {
+                    templateUrl : "views//home/home.view.html"
+                })
+                .when("/profile", {
+                    templateUrl : "views/users/profile.view.html"
+                })
+                .when("/admin", {
+                    templateUrl : "views//admin/admin.view.html"
+                })
+                .when("/forms", {
+                    templateUrl : "views//forms/forms.view.html"
+                })
+                .when("/register", {
+                    templateUrl : "views//users/register.view.html"
+                })
+                .when("/login", {
+                    templateUrl : "views//users/login.view.html"
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+    });
 
     function testController($scope) {
         $scope.home = "Testing angular";
