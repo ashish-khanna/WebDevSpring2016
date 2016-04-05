43a5f55450efaa07dd8d5fb02ce6238e0239546d
Ashish Khanna <singh.surv@neu.husky.edu>
2016-04-05 16:34:20 -0400

    Event Details Page - Google Map Integrated

--- public/project/eventManagement/views/event/eventDetail.controller.js
+++ public/project/eventManagement/views/event/eventDetail.controller.js
@@ -8,7 +8,6 @@
 
     function EventController($rootScope, $scope, $location, $routeParams, EventService){
         var vm = this;
-        console.log("EventController");
 
         var eventId = $routeParams.event;
 
@@ -19,6 +18,22 @@
                     function(doc){
                         vm.event = doc;
                         $scope.event = vm.event;
+
+                        var mapOptions = {
+                            zoom: 13,
+                            center: new google.maps.LatLng(vm.event.venue.latitude, vm.event.venue.longitude),
+                            mapTypeId: google.maps.MapTypeId.TERRAIN,
+                        }
+                        $scope.map = new google.maps.Map(document.getElementById('eventMap'), mapOptions);
+
+                        var marker = new google.maps.Marker({
+                               map: $scope.map,
+                                    position: new google.maps.LatLng(vm.event.venue.latitude, vm.event.venue.longitude),
+                                    title: vm.event.name.text,
+                                    animation: "Animation.BOUNCE",
+                                    icon: "https://upload.wikimedia.org/wikipedia/commons/9/92/Map_marker_icon_%E2%80%93_Nicolas_Mollet_%E2%80%93_Home_%E2%80%93_People_%E2%80%93_Default.png"
+                                });
+
                     }
                 )
         }
